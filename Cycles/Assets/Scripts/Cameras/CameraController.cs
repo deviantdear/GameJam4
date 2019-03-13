4b9efba6116cd7aab0b358f0ec03973195e53928
Valarie Adams <deviantdear@gmail.com>
2019-03-12 23:54:26 -0700

    Update CameraController.cs
    added Yaw. Yee Haw.

--- Cycles/Assets/Scripts/Cameras/CameraController.cs
+++ Cycles/Assets/Scripts/Cameras/CameraController.cs
@@ -8,11 +8,14 @@ public class CameraController : MonoBehaviour
 
     public Vector3 offset;
     public float zoomSpeed = 4f;
+    public float yawSpeed = 100f;
+
     private float currentZoom =10f;
     public float minZoom = 5f;
     public float maxZoom = 15f;
-
+    private float currentYaw = 0f;
     public float pitch = 2f;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +27,7 @@ public class CameraController : MonoBehaviour
     {
         currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
         currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
     }
 
     // Late Update is called once per frame after all other items in the frame have loaded
@@ -31,5 +35,6 @@ public class CameraController : MonoBehaviour
     {
         transform.position = target.position - offset * currentZoom;
         transform.LookAt(target.position + Vector3.up * pitch);
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
     }
 }
