0c3b6316cfe16e9eb4195a063b01d02d5f4f89d9
Valarie Adams <deviantdear@gmail.com>
2019-03-12 23:46:41 -0700

    Skyboxes, Scripts
    Added 2 Skyboxes Dark and Light, created more movement and camera scripts

--- Cycles/Assets/Scripts/Cameras/CameraController.cs
+++ Cycles/Assets/Scripts/Cameras/CameraController.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    public Transform target;
 
-    private Vector3 offset;
-    private Vector3 distanceFromBall = new Vector3 (0, 1.5f, 0);
+    public Vector3 offset;
+    public float zoomSpeed = 4f;
+    private float currentZoom =10f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
+
+    public float pitch = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = (transform.position - player.transform.position) + distanceFromBall;
+        
+    }
+
+     void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
     }
 
     // Late Update is called once per frame after all other items in the frame have loaded
     void LateUpdate()
-    { 
-        transform.position = (player.transform.position + offset * 0.2f *player.transform.localScale.magnitude);
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
     }
 }
--- Cycles/Assets/Scripts/Characters/Chick.cs
+++ Cycles/Assets/Scripts/Characters/Chick.cs
@@ -2,17 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMover))]
 public class Chick : MonoBehaviour
 {
+    //Layermask prevents us from targeting objects we don't mean to when moving
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMover mover;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
+        mover = GetComponent<PlayerMover>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            TravelToPoint();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            FocusOnObject();
+        }
+    }
+
+    void TravelToPoint()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        //finds mouse click and filters to layer desired
+        if (Physics.Raycast(ray, out hit, 100, movementMask))
+        {
+            Debug.Log("Object hit=" + hit.collider.name + " " + hit.point);
+            //Moving player to mouse click location
+            mover.MoveToPoint(hit.point);
+
+            //Stop focusing any objects
+
+        }
+    }
+
+    void FocusOnObject()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        //finds mouse click and filters to layer desired
+        if (Physics.Raycast(ray, out hit, 100, movementMask))
+        {
+            //Check to see if you can interact
+            //set as focus
+
+        }
     }
 }
