3dc14bd20a4be25b7d4040d958d7ad666298dcb0
Valarie Adams <deviantdear@gmail.com>
2019-03-13 13:12:53 -0700

    Inventory and pick up item scripts working

--- Cycles/Assets/Scripts/Interactables/ItemPickup.cs
+++ Cycles/Assets/Scripts/Interactables/ItemPickup.cs
@@ -3,5 +3,22 @@ using UnityEngine;
 
 public class ItemPickup : Interactable
 {
-    
+    public Item item;
+    public override void Interact()
+    {
+        base.Interact();
+        PickUp();
+        
+    }
+
+    void PickUp()
+    {
+        Debug.Log("Picking Up " + item.name);
+        //Add item to inventory or update UI, XP etc.
+        bool wasPickedUp = Inventory.instance.Add(item);
+        if (wasPickedUp)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
