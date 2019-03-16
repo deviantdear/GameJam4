bb03a9e936ba292961ac1a0892563655aaeff04b
Valarie Adams <deviantdear@gmail.com>
2019-03-15 23:46:32 -0700

    Added player stats and character stats
    Added to the "Final Scene" File

---
+++ Cycles/Assets/Scripts/Characters/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AttackManager.instance.onAttackChanged += onAttackChanged;
+    }
+
+    void OnAttackChanged(AttackModifier newAttack, AttackModifier oldAttack)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
