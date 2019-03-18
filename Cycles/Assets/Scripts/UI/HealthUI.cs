689826c36157f83bcd7ccb8ee0a6b454c0e38c49
Valarie Adams <deviantdear@gmail.com>
2019-03-18 13:11:10 -0700

    Adding floating health UI scripts to give feedback to player about enemy health and damage

---
+++ Cycles/Assets/Scripts/UI/HealthUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    // For Health Bar that appears above the object
+
+    public GameObject uiPrefab;
+    public Transform target;
+
+    Transform ui;
+    Image healthSlider;
+    Transform cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform; // references camera in scene
+
+        foreach(Canvas C in FindObjectsOfType<Canvas>())
+        {
+            if(C.renderMode == RenderMode.WorldSpace)
+            {
+                ui = Instantiate(uiPrefab, C.transform).transform;
+                healthSlider = ui.GetChild(0).GetComponent<Image>(); //Finds image child of prefab
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        ui.position = target.position; //Moves with player
+        ui.forward = -cam.forward; //
+    }
+}
